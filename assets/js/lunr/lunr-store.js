32d4b1ac1dcd5abc77a99c6d593ef36c7e267673
Fabien Evrard <fa.evrard@gmail.com>
2024-08-16 11:24:34 -0500

    Testing private plugin

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,84 +1,13 @@
----
-layout: none
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | relative_url | jsonify }},
-        "teaser": {{ teaser | relative_url | jsonify }}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}{%- if site.lunr.search_within_pages -%},
-  {%- assign pages = site.pages | where_exp:'doc','doc.search != false' -%}
-  {%- for doc in pages -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-  {
-    "title": {{ doc.title | jsonify }},
-    "excerpt":
-        {%- if site.search_full_content == true -%}
-          {{ doc.content | newline_to_br |
-            replace:"<br />", " " |
-            replace:"</p>", " " |
-            replace:"</h1>", " " |
-            replace:"</h2>", " " |
-            replace:"</h3>", " " |
-            replace:"</h4>", " " |
-            replace:"</h5>", " " |
-            replace:"</h6>", " "|
-          strip_html | strip_newlines | jsonify }},
-        {%- else -%}
-          {{ doc.content | newline_to_br |
-            replace:"<br />", " " |
-            replace:"</p>", " " |
-            replace:"</h1>", " " |
-            replace:"</h2>", " " |
-            replace:"</h3>", " " |
-            replace:"</h4>", " " |
-            replace:"</h5>", " " |
-            replace:"</h6>", " "|
-          strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-        {%- endif -%}
-      "url": {{ doc.url | absolute_url | jsonify }}
-  }{%- unless forloop.last and l -%},{%- endunless -%}
-  {%- endfor -%}
-{%- endif -%}]
+var store = [{
+        "title": "New website",
+        "excerpt":"Welcome to the new website of the Computational Multiphase Flow Research group at the University of Illinois Urbana-Champaign. We are located in the Department of Aerospace Engineering and we develop numerical methods and physical models to study and predict multiphase flows with applications in aerospace and beyond.  ","categories": [],
+        "tags": [],
+        "url": "/new-website/",
+        "teaser": null
+      },{
+        "title": "NASA Early Stage Innovation grant",
+        "excerpt":"Fabien Evrard and lead-PI Laura Villafañe, together with Jesse Capecelatro at Michigan, were recently awarded a NASA Early Stage Innovation grant. This collaboration aims to produce new physics-based models for predicting plume-surface interactions, as they occur during lunar or martian landing due to engine exhausts blowing away large amounts of...","categories": [],
+        "tags": [],
+        "url": "/nasa-esi/",
+        "teaser": null
+      }]
